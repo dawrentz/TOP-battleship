32ffca908714a377d88650147d0efcd28bae7545
dawrentz <156712569+dawrentz@users.noreply.github.com>
2025-01-23 13:18:54 -0600

    Finish initial Gameboard class with tests.
    Ready for Player class.

--- src/gameboardMod.js
+++ src/gameboardMod.js
@@ -13,6 +13,78 @@ export class Gameboard {
   constructor() {
     this.pointer = null;
     this.board = this.createGameboard();
+    this.liveShips = 0;
+  }
+
+  receiveAttack(coordSet) {
+    const xCoord = coordSet[0];
+    const yCoord = coordSet[1];
+    const thisSquare = this.board[xCoord][yCoord];
+
+    //handle previous attack
+    if (thisSquare.hasHit === true)
+      throw new Error("Square has already been attacked");
+
+    //handle ship attack
+    if (thisSquare.ship !== null) {
+      thisSquare.ship.hit();
+      this.checkForSunk(thisSquare.ship);
+    }
+
+    //update square
+    thisSquare.hasHit = true;
+
+    //check for game over
+    if (this.checkForGameOver === true) {
+      //end game
+    }
+  }
+
+  checkForGameOver() {
+    return this.liveShips === 0 ? true : false;
+  }
+
+  checkForSunk(ship) {
+    if (ship.sunk === true) this.liveShips--;
+  }
+
+  placeShip(shipLength, headCoords, isHorz) {
+    const possCoords = this.getAllShipCoords(shipLength, headCoords, isHorz);
+
+    const newShip = new shipMod.Ship(shipLength);
+
+    possCoords.forEach((coordSet) => {
+      const xCoord = coordSet[0];
+      const yCoord = coordSet[1];
+      this.board[xCoord][yCoord].ship = newShip;
+    });
+
+    this.liveShips++;
+  }
+
+  //needs check for out of bounds and collisions
+  getAllShipCoords(shipLength, headCoordsArr, isHorz) {
+    let allCoords = [];
+    const headXcoord = headCoordsArr[0];
+    const headYcoord = headCoordsArr[1];
+
+    //horz placement
+    if (isHorz === true) {
+      for (let i = 0; i < shipLength; i++) {
+        allCoords.push([headXcoord + i, headYcoord]);
+      }
+    }
+    //vert placement
+    else if (isHorz === false) {
+      for (let i = 0; i < shipLength; i++) {
+        allCoords.push([headXcoord, headYcoord + i]);
+      }
+    }
+    //error handle
+    else {
+      throw new Error("isHorz must be boolen");
+    }
+    return allCoords;
   }
 
   createGameboard() {
@@ -24,8 +96,7 @@ export class Gameboard {
     //add Square objects to make columns
     board.forEach((row) => {
       for (let i = 0; i < numOfCols; i++) {
-        const newSquare = new Square();
-        row.push(newSquare);
+        row.push(new Square());
       }
     });
 
@@ -35,12 +106,18 @@ export class Gameboard {
 
 export class Square {
   constructor() {
-    this.hasShip = false;
     this.hasHit = false;
+    this.ship = null;
   }
 }
 
 // ====================================== Lessor Functions ====================================== //
 
+// ====================================== testing ====================================== //
+
 const testGameboard = new Gameboard();
-console.log(testGameboard.board);
+const newSquare = new Square();
+
+// console.log(testGameboard.placeShip(3, [1, 1], true));
+// console.log(newSquare.ship);
+// console.log(testGameboard.board);
--- src/gameboardMod.test.js
+++ src/gameboardMod.test.js
@@ -1,10 +1,132 @@
 //gameboardMod.test.js
 
+// ====================================== init/declare ====================================== //
+
 import * as gameboardMod from "./gameboardMod.js";
 
-it("make a Square", () => {
+// ====================================== Square ====================================== //
+
+it("makes a Square", () => {
   const newSquare = new gameboardMod.Square();
 
-  expect(newSquare.hasShip).toBe(false);
+  expect(newSquare.ship).toBe(null);
   expect(newSquare.hasHit).toBe(false);
+});
+
+it("makes a Gameboard", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+
+  expect(newGameboard.board.length).toBe(10);
+
+  newGameboard.board.forEach((row) => {
+    expect(row.length).toBe(10);
+  });
+});
+
+// ====================================== Get Coords ====================================== //
+//patter is: possCoords[choose which set of coords][0 for the x-coord, 1 for the y-coord]
+
+it("return correct HORZ possible coords for ship placement", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  const possCoords = newGameboard.getAllShipCoords(3, [0, 0], true);
+
+  //x-coords
+  expect(possCoords[0][0]).toBe(0);
+  expect(possCoords[1][0]).toBe(1);
+  expect(possCoords[2][0]).toBe(2);
+  //y-coords
+  expect(possCoords[0][1]).toBe(0);
+  expect(possCoords[1][1]).toBe(0);
+  expect(possCoords[2][1]).toBe(0);
+});
+
+it("return correct VERT possible coords for ship placement", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  const possCoords = newGameboard.getAllShipCoords(3, [0, 0], false);
+
+  //x-coords
+  expect(possCoords[0][0]).toBe(0);
+  expect(possCoords[1][0]).toBe(0);
+  expect(possCoords[2][0]).toBe(0);
+  //y-coords
+  expect(possCoords[0][1]).toBe(0);
+  expect(possCoords[1][1]).toBe(1);
+  expect(possCoords[2][1]).toBe(2);
+});
+
+// ====================================== Place Ship ====================================== //
+
+it("places ship HORZ", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  //simulate user input
+  const userCoords = [0, 0];
+  const shipLength = 3;
+
+  //horizontal placement
+  newGameboard.placeShip(shipLength, userCoords, true);
+  for (let i = 0; i < shipLength; i++) {
+    const tempSquare = newGameboard.board[userCoords[0] + i][userCoords[1]];
+    expect(tempSquare.ship.hits).not.toBe(undefined);
+  }
+});
+
+it("places ship VERT", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  //simulate user input
+  const userCoords = [0, 0];
+  const shipLength = 3;
+
+  //vertical placement
+  newGameboard.placeShip(shipLength, userCoords, false);
+  for (let i = 0; i < shipLength; i++) {
+    const tempSquare = newGameboard.board[userCoords[0]][userCoords[1] + i];
+    expect(tempSquare.ship.hits).not.toBe(undefined);
+  }
+});
+
+// ====================================== Attack ====================================== //
+
+it("attacks empty space", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  expect(newGameboard.board[0][0].hasHit).toBe(false);
+
+  newGameboard.receiveAttack([0, 0]);
+  expect(newGameboard.board[0][0].hasHit).toBe(true);
+});
+
+it("attacks ship", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  newGameboard.placeShip(3, [0, 0], true);
+  expect(newGameboard.board[0][0].ship.hits).toBe(0);
+
+  newGameboard.receiveAttack([0, 0]);
+  expect(newGameboard.board[0][0].ship.hits).toBe(1);
+  //will not accept repeat attacks
+  newGameboard.receiveAttack([1, 0]);
+  expect(newGameboard.board[0][0].ship.hits).toBe(2);
+});
+
+it("sinks ship", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  newGameboard.placeShip(3, [0, 0], true);
+
+  newGameboard.receiveAttack([0, 0]);
+  newGameboard.receiveAttack([1, 0]);
+  newGameboard.receiveAttack([2, 0]);
+
+  expect(newGameboard.board[0][0].ship.sunk).toBe(true);
+});
+
+it("sinks all ships", () => {
+  const newGameboard = new gameboardMod.Gameboard();
+  newGameboard.placeShip(3, [0, 0], true);
+
+  expect(newGameboard.liveShips).toBe(1);
+
+  newGameboard.receiveAttack([0, 0]);
+  newGameboard.receiveAttack([1, 0]);
+  newGameboard.receiveAttack([2, 0]);
+
+  expect(newGameboard.liveShips).toBe(0);
+  expect(newGameboard.checkForGameOver()).toBe(true);
 });
